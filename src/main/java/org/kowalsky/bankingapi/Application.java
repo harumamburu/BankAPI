edeb0cbe67a9564943737aa6558473c817e17587
Nikolay_Kovalev <nikolay.kovalev92@gmail.com>
2022-03-14 13:52:08 +0300

    Add Dagger injection

--- src/main/java/org/kowalsky/bankingapi/Application.java
+++ src/main/java/org/kowalsky/bankingapi/Application.java
@@ -1,14 +1,23 @@
 package org.kowalsky.bankingapi;
 
+import dagger.Component;
 import org.kowalsky.bankingapi.controller.CurrenciesController;
+
+import javax.inject.Singleton;
 
 import static spark.Spark.get;
 import static spark.Spark.path;
 
 public class Application {
 
+    @Singleton
+    @Component
+    public interface BankingAPI {
+        CurrenciesController currenciesAPI();
+    }
+
     public static void main(String[] args) {
-        CurrenciesController currenciesController = new CurrenciesController();
+        CurrenciesController currenciesController = DaggerApplication_BankingAPI.create().currenciesAPI();
 
         path("/openbanking", () -> {
             path("/v1", () -> {
--- src/main/java/org/kowalsky/bankingapi/controller/CurrenciesController.java
+++ src/main/java/org/kowalsky/bankingapi/controller/CurrenciesController.java
@@ -1,6 +1,13 @@
 package org.kowalsky.bankingapi.controller;
 
+import javax.inject.Inject;
+
 public class CurrenciesController {
+
+    @Inject
+    public CurrenciesController() {
+
+    }
 
     public String getCurrencies() {
         return "Hello World!";
